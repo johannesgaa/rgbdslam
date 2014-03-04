9bd0e3017ae5549942e48292a95856ad260b953e
imesadmin <imesadmin@imesros12.(none)>
2014-03-04 17:08:49 +0100

    commented

--- src/main.cpp
+++ src/main.cpp
@@ -127,6 +127,8 @@ int main(int argc, char** argv)
   QObject::connect(&app, SIGNAL(aboutToQuit()), &qtRos, SLOT(quitNow()));
   QObject::connect(&qtRos, SIGNAL(rosQuits()), &app, SLOT(quit()));
 
+
+  //blub
   qtRos.start();// Run main loop.
   app.exec();
   //if(ros::ok()) ros::shutdown();//If not yet done through the qt connection
